053f1b8c3a9aa0c1202b1f581cdee3aecc45f29d
amir <amiralizadeh9480@gmail.com>
2020-04-26 01:50:18 +0430

    Update view.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -86,14 +86,7 @@
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="#">این ابزار فوق العادس!</a>
                 </div>
             </div>
         </div>
